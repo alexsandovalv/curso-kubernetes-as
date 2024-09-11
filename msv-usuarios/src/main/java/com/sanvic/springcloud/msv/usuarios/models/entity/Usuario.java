a545f3608cdd4046c59e8a0a7c692c1ad649275f
Alexander Sandoval <alexander.sandoval@hpe.com>
2024-09-10 21:28:39 -0500

    Start application

---
+++ msv-usuarios/src/main/java/com/sanvic/springcloud/msv/usuarios/models/entity/Usuario.java
@@ -0,0 +1,18 @@
+package com.sanvic.springcloud.msv.usuarios.models.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+
+
+}
